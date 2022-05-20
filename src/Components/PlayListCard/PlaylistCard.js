67040560e53c7ecbae920ef53dd0fe29f68c1218
neetukumari4858 <neetu21@navgurukul.org>
2022-05-20 19:35:11 +0530

    refector:minor changes

--- src/Components/PlayListCard/PlaylistCard.js
+++ src/Components/PlayListCard/PlaylistCard.js
@@ -1,29 +1,37 @@
-import "./PlaylistCard.css"
-import { usePlayList } from "../../Context/PlayListContext"
+import "./PlaylistCard.css";
+import { usePlayList } from "../../Context/PlayListContext";
 import { Link } from "react-router-dom";
 
 const PlaylistCard = () => {
-    const { Playlist, PlayListDispatch } = usePlayList();
-    return (
-        <>
-            {Playlist.map((item) => {
-                return (
-                    <div className="playlistCard" key={item._id}>
-                        <div className="delete-icon-and-text">
-                            <Link to={`/PlaylistPage/${item._id}`} className="playlistcardLink" >
-                                <h2 className="cardTitle cardText">{item.title}</h2>
-                                <i className='fa fa-trash-o delete ' onClick={(e) => 
-                                    {
-                                        e.preventDefault()
-                                        PlayListDispatch({ type: "REMOVE_PLAYLIST", payload: item._id })}}></i>
-                            </Link>
-                        </div>
-                        <h6 className="videoLength cardText">{item.videos.length} video</h6>
-                    </div>
-                )
-            })
-            }
-        </>
-    )
-}
-export { PlaylistCard }
+  const { Playlist, PlayListDispatch } = usePlayList();
+  return (
+    <>
+      {Playlist.map((item) => {
+        return (
+          <div className="playlistCard" key={item._id}>
+            <div className="delete-icon-and-text">
+              <Link
+                to={`/PlaylistPage/${item._id}`}
+                className="playlistcardLink"
+              >
+                <h2 className="cardTitle ">{item.title}</h2>
+                <i
+                  className="fa fa-trash-o delete "
+                  onClick={(e) => {
+                    e.preventDefault();
+                    PlayListDispatch({
+                      type: "REMOVE_PLAYLIST",
+                      payload: item._id,
+                    });
+                  }}
+                ></i>
+              </Link>
+            </div>
+            <h6 className="videoLength cardText">{item.videos.length} video</h6>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+export { PlaylistCard };
