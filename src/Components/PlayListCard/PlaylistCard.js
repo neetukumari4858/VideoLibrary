0fbfee2143707500d48b7be7e1a445f56a4237aa
neetukumari4858 <90403664+neetukumari4858@users.noreply.github.com>
2022-06-15 23:17:02 +0530

    refector:minor changes

--- src/Components/PlayListCard/PlaylistCard.js
+++ src/Components/PlayListCard/PlaylistCard.js
@@ -1,8 +1,8 @@
 import './PlaylistCard.css'
 import { usePlayList, useAuth } from '../../Context/index'
 import { Link } from 'react-router-dom'
-import { deletePlaylist,getPlaylist } from './../../services/index'
-import { useEffect } from "react";
+import { deletePlaylist, getPlaylist } from './../../services/index'
+import { useEffect } from 'react'
 
 const PlaylistCard = () => {
   const { Playlist, PlayListDispatch } = usePlayList()
@@ -10,22 +10,29 @@ const PlaylistCard = () => {
   const { userDetail } = useAuth()
   const { token } = userDetail
 
-
   useEffect(() => getPlaylist(token, PlayListDispatch), [])
 
   const deletePlaylistHandler = (playlistId) => {
     deletePlaylist(playlistId, token, PlayListDispatch)
   }
 
+  const singlePlaylistHandler = (videos) => {
+    PlayListDispatch({ type: 'VIDEO_TO_SINGLE_PLAYLIST_PAGE', payload: videos })
+  }
+
   return (
     <>
       {playlists.map((item) => {
+        {console.log(playlists,"playlist");}
         return (
           <div className="playlistCard" key={item.videos}>
+            {/* {console.log(item.videos,"yi")} */}
             <div className="playlist_text">
               <Link
                 to={`/PlaylistPage/${item._id}`}
                 className="playlistcardLink"
+                
+                onClick={() => singlePlaylistHandler(item.videos)}
               >
                 <h2 className="playlistName">{item.title}</h2>
               </Link>
--- src/Page/PlayList/Playlist.js
+++ src/Page/PlayList/Playlist.js
@@ -1,13 +1,14 @@
 import "./Playlist.css"
 import {PlaylistCard} from "./../../Components/PlayListCard/PlaylistCard"
-import { usePlayList } from "../../Context/PlayListContext"
+import { usePlayList } from "../../Context/index";
 const Playlist = () => {
-    const {Playlist}=usePlayList ()
+    const {Playlist}=usePlayList()
     const {playlists}=Playlist
     
     return (
         <div className="Playlist-Page-container">
             <h1 className="Playlist">Playlist Videos ({playlists.length})</h1>
+            
             {playlists.length>0 ?
             (<div className="section">
                 <PlaylistCard/>
